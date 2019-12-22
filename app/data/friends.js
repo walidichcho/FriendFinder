9a4bf129d59c36a12ac0fdcea514ffa8a354515d
Naima <nbensassi@hotmail.com>
2019-12-21 22:08:36 -0500

    add all files still need to update

---
+++ app/data/friends.js
@@ -0,0 +1,121 @@
+var friends = [
+
+    {
+        "name": "Ahmed",
+        "photo": "https://d1srin71gt7f9x.cloudfront.net/wp-content/uploads/2015/03/jennifer-lopez-selena-movie-1997-photo-FC-476x320.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+
+    {
+        "name": "Wiam",
+        "photo": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-89851349-1564747627.jpg",
+        "scores": [
+            3,
+            1,
+            3,
+            3,
+            3,
+            1,
+            2,
+            3,
+            3,
+            3
+        ]
+    },
+    {
+        "name": "John",
+        "photo": "https://images2.minutemediacdn.com/image/upload/c_fill,w_912,h_516,f_auto,q_auto,g_auto/shape/cover/sport/france-v-germany-uefa-nations-league-a-5bca0ac66f0e5b6500000001.jpg",
+        "scores": [
+            3,
+            1,
+            3,
+            3,
+            3,
+            1,
+            2,
+            3,
+            3,
+            3
+        ]
+    },
+    {
+        "name": "Edward",
+        "photo": "https://specials-images.forbesimg.com/imageserve/1170388496/960x0.jpg",
+        "scores": [
+            5,
+            4,
+            4,
+            4,
+            4,
+            1,
+            2,
+            3,
+            4,
+            3
+        ]
+    },
+    {
+        "name": "Frank",
+        "photo": "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+        "scores": [
+            3,
+            1,
+            3,
+            2,
+            2,
+            1,
+            2,
+            2,
+            4,
+            2
+        ]
+    },
+    {
+        "name": "stephanie",
+        "photo": "https://s.yimg.com/ny/api/res/1.2/EEA6ecfEeZlD3zn4CR0rAA--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9MTI4MDtoPTk2MA--/http://media.zenfs.com/en-US/homerun/etonline.tv/29bad6b9d4aaa50b6e085e271ec395a8",
+        "scores": [
+            2,
+            2,
+            2,
+            2,
+            2,
+            1,
+            2,
+            2,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "stephanie",
+        "photo": "https://media1.fdncms.com/orlando/imager/u/blog/25665033/gal_jlo_headshot_by_gabriel_goldman.jpg",
+        "scores": [
+            5,
+            2,
+            2,
+            5,
+            2,
+            5,
+            5,
+            5,
+            4,
+            5
+        ]
+    }
+
+];
+
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,87 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var friends = require("../data/friends.js");
+
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function (app) {
+    // API GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases when a user visits a link
+    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+    // ---------------------------------------------------------------------------
+
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+    });
+
+
+
+    // API POST Requests
+    // Below code handles when a user submits a form and thus submits data to the server.
+    // In each of the below cases, when a user submits form data (a JSON object)
+    // ...the JSON is pushed to the appropriate JavaScript array
+    // (ex. User fills out a reservation request... this data is then sent to the server...
+    // Then the server saves the data to the tableData array)
+    // ---------------------------------------------------------------------------
+
+    app.post("/api/friends", function (req, res) {
+        console.log(req.body.scores);
+
+        // Receive user details (name, photo, scores)
+        var user = req.body;
+
+        // parseInt for scores
+        for (var i = 0; i < user.scores.length; i++) {
+            user.scores[i] = parseInt(user.scores[i]);
+        }
+
+        // default friend match is the first friend but result will be whoever has the minimum difference in scores
+        var bestFriendIndex = 0;
+        var minimumDifference = 40;
+
+        // in this for-loop, start off with a zero difference and compare the user and the ith friend scores, one set at a time
+        //  whatever the difference is, add to the total difference
+        for (var i = 0; i < friends.length; i++) {
+            var totalDifference = 0;
+            for (var j = 0; j < friends[i].scores.length; j++) {
+                var difference = Math.abs(user.scores[j] - friends[i].scores[j]);
+                totalDifference += difference;
+            }
+
+            // if there is a new minimum, change the best friend index and set the new minimum for next iteration comparisons
+            if (totalDifference < minimumDifference) {
+                bestFriendIndex = i;
+                minimumDifference = totalDifference;
+            }
+        }
+
+        // after finding match, add user to friend array
+        friends.push(user);
+
+        // send back to browser the best friend match
+        res.json(friends[bestFriendIndex]);
+    });
+};
+
+
+    // ---------------------------------------------------------------------------
+    // I added this below code so you could clear out the table while working with the functionality.
+    // Don"t worry about it!
+
+//     app.post("/api/clear", function (req, res) {
+//         // Empty out the arrays of data
+//         friends.length = 0;
+
+
+//         res.json({ ok: true });
+//     });
+// };
